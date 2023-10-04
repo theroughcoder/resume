59c515c5e87280263201c314177b4f2b78bf854b
theroughcoder <sanchitpandeyofficial@gmail.com>
2023-10-04 23:56:39 +0530

    first commit

---
+++ src/projects.js
@@ -0,0 +1,37 @@
+export const projects = [
+
+    {name : "FacebookClone",
+        img : "./images/fb-profile.png",
+        link : "https://facebook-clone-two-livid.vercel.app/",
+        des : "It's a project that is built with the help of react, express, node and mongobd. This project has used redux for storing the variable globally."
+    },
+    {name : "Bluesdare",
+        img : "./images/bluesdare.png",
+        link : "https://bluesdare.vercel.app/",
+        des : "Bluesdare is e-commerce web project that is a clone of amazon. The UI of the website is fully responsive for desktop, tablet, and mobile."
+    }
+    ,
+    {name : "Employee Review System",
+        img : "./images/ers.png",
+        link : "https://employee-review-system-1zkx.onrender.com/user/sign-in",
+        des : "This is an application that provides employees with opportunities to give feedback to each other. "
+    }
+    ,
+    {name : "Snake Game",
+        img : "./images/snake2.png",
+        link : "https://theroughcoder.github.io/snake-game/",
+        des : "Snake game project, developed using HTML, CSS and Javascript. Playing this game will remind you of your childhood. "
+    }
+    ,
+    {name : "Albums List",
+        img : "./images/al (1).png",
+        link : "https://albums-list-chi.vercel.app/",
+        des : "This application is designed to represent albums elegantly. It is built with the help of React and the UI of the app is also quite clean.  "
+    }
+    ,
+    {name : "Placement Cell",
+        img : "./images/pc.png",
+        link : "https://baby-blue-grasshopper-tutu.cyclic.app/user/sign-in",
+        des : "This application is designed to help the placement cell in scheduling and managing student interviews.  "
+    }
+]
